74c9edbd0b0b6ed3def4ee991aa03b539c4b82e3
SnkSynthesis <63564282+SnkSynthesis@users.noreply.github.com>
2021-06-28 18:27:57 -0700

    Add static files + templating for svelte frontend

--- {{cookiecutter.project_slug}}/backend/app.py
+++ {{cookiecutter.project_slug}}/backend/app.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 
 from . import api_v1
-
 
 app = FastAPI(
     title="{{cookiecutter.project_name}}", version="{{cookiecutter.project_version}}"
 )
+
 app.include_router(api_v1.router, prefix="/api/v1")
+
+templates = Jinja2Templates(directory="frontend/public")
+app.mount("/static", StaticFiles(directory="frontend/public"))
 
 
 @app.get("/")
-async def root():
-    return {"title": "{{cookiecutter.project_name}}", "version": app.version}
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
