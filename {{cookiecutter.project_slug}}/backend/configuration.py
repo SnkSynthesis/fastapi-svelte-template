5be6934e359ae697628b6af22587f305777e930b
SnkSynthesis <63564282+SnkSynthesis@users.noreply.github.com>
2021-08-18 20:50:40 -0700

    Start authentication code

--- {{cookiecutter.project_slug}}/backend/configuration.py
+++ {{cookiecutter.project_slug}}/backend/configuration.py
@@ -5,5 +5,10 @@ class Configuration(BaseSettings):
     db_url: str = "sqlite:///./database.db"
     test_db_url: str = "sqlite:///./test.db"
 
+    # openssl rand -hex 32
+    secret_key: str = "c1db475aa41be1b794cbb9881e87c89ad632c698ef84862cae400f86dea54c2b"
+    algorithm: str = "HS256"
+    token_expire: int = 30  # minutes
+
 
 config = Configuration()
