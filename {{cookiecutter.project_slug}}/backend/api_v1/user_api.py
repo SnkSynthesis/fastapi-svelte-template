b20f0ce41850e10815cca834abf9d31e8b439162
SnkSynthesis <63564282+SnkSynthesis@users.noreply.github.com>
2021-08-18 20:50:24 -0700

    Use username as pk and fastapi.status for user api

--- {{cookiecutter.project_slug}}/backend/api_v1/user_api.py
+++ {{cookiecutter.project_slug}}/backend/api_v1/user_api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
 from ..crud.user_crud import UserCRUD, User, UserIn
 from ..crud.deps import get_crud_obj
@@ -8,31 +8,42 @@ from starlette.responses import Response
 
 router = APIRouter(tags=["User API"])
 
-@router.get("/{id}", response_model=User)
-async def get_one_users(id: int, user_crud: UserCRUD = Depends(get_crud_obj(UserCRUD))) -> Any:
-    user = await user_crud.get_one(id)
+
+@router.get("/{username}", response_model=User)
+async def get_one_user(
+    username: str, user_crud: UserCRUD = Depends(get_crud_obj(UserCRUD))
+) -> Any:
+    user = await user_crud.get_one(username)
     if user is None:
-        raise HTTPException(detail="Not Found", status_code=404)
+        raise HTTPException(detail="Not Found", status_code=status.HTTP_404_NOT_FOUND)
     return user
 
+
 @router.post("/", response_model=User)
-async def create_user(user: UserIn, user_crud: UserCRUD = Depends(get_crud_obj(UserCRUD))) -> Any:
-    last_record_id = await user_crud.create(user)
-    return await user_crud.get_one(last_record_id)
+async def create_user(
+    user: UserIn, user_crud: UserCRUD = Depends(get_crud_obj(UserCRUD))
+) -> Any:
+    last_record_username = await user_crud.create(user)
+    return await user_crud.get_one(last_record_username)
 
-@router.put("/{id}", response_model=User)
-async def update_user(id: int, updated_user: UserIn, user_crud: UserCRUD = Depends(get_crud_obj(UserCRUD))) -> Any:
-    user = await user_crud.get_one(id)
+
+@router.put("/", response_model=User)
+async def update_user(
+    updated_user: UserIn, user_crud: UserCRUD = Depends(get_crud_obj(UserCRUD))
+) -> Any:
+    user = await user_crud.get_one(updated_user.username)
     if user is None:
-        raise HTTPException(detail="Not Found", status_code=404)
-    await user_crud.update(id, updated_user)
-    return {"id": id, **updated_user.dict()}
+        raise HTTPException(detail="Not Found", status_code=status.HTTP_404_NOT_FOUND)
+    await user_crud.update(updated_user.username, updated_user)
+    return {**updated_user.dict()}
 
 
-@router.delete("/{id}")
-async def delete_user(id: int, user_crud: UserCRUD = Depends(get_crud_obj(UserCRUD))) -> Any:
-    user = await user_crud.get_one(id)
+@router.delete("/{username}")
+async def delete_user(
+    username: str, user_crud: UserCRUD = Depends(get_crud_obj(UserCRUD))
+) -> Any:
+    user = await user_crud.get_one(username)
     if user is None:
-        raise HTTPException(detail="Not Found", status_code=404)
-    await user_crud.delete(id)
-    return Response(status_code=204)
+        raise HTTPException(detail="Not Found", status_code=status.HTTP_404_NOT_FOUND)
+    await user_crud.delete(username)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
